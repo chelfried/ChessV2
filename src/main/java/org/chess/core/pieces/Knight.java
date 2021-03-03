e78314a3eedb0be49546407f5569d8b36386b190
Helfried <helfried.git@gmail.com>
2021-03-03 16:37:03 +0200

    bugfixes and refactoring

--- src/main/java/org/chess/core/pieces/Knight.java
+++ src/main/java/org/chess/core/pieces/Knight.java
@@ -7,20 +7,9 @@ public class Knight extends _Piece {
     public static String possibleN(long knight) {
         StringBuilder PseudoMoves = new StringBuilder();
         long i = knight & - knight;
-        long movement;
         while (i != 0) {
             int idx = numberOfTrailingZeros(i);
-            if (idx > 18) {
-                movement = 0xa1100110aL << (idx - 18);
-            } else {
-                movement = 0xa1100110aL >> (18 - idx);
-            }
-            if (idx % 8 < 4) {
-                movement &= 0x3f3f3f3f3f3f3f3fL & antiPlayerPieces;
-            } else {
-                movement &= 0xfcfcfcfcfcfcfcfcL & antiPlayerPieces;
-            }
-            moveUtil(PseudoMoves, movement, idx,'d');
+            moveUtil(PseudoMoves, knightAttack[idx] & antiPlayerPieces, idx, 'd');
             knight &= ~ i;
             i = knight & - knight;
         }
